b5f73e8696d795b85920e1d0d035dce1b2425170
Elliott <Elliott2.pugh@live.uwe.ac.uk>
2024-04-19 13:09:17 +0100

    new stuff
    not finished

---
+++ P3DE_Project/Assets/Scripts/BoxScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxScript : MonoBehaviour
+{
+    public Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.Play("opening_anim");
+    }
+}
